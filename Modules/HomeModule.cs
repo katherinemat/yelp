6a5f7a230037def4dcc2efd383fc6a205ac3ce73
Kaz Matthews and Minh Phuong <kaz+minh@>
2017-02-22 11:55:39 -0800

    add routes for restaurants and cuisines with corresponding views.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,7 +13,29 @@ namespace Yelp
       };
       Get["/cuisines"] = _ =>{
         List<Cuisine> allCuisines = Cuisine.GetAll();
-        return View["cuisines.cshtml",allCuisines];
+        return View["cuisines.cshtml", allCuisines];
+      };
+      Post["/cuisines/new"] = _ => {
+        string cuisineName = Request.Form["cuisine"];
+        Cuisine newCuisine = new Cuisine(cuisineName);
+        newCuisine.Save();
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["cuisines.cshtml", allCuisines];
+      };
+      Get["/restaurants"] = _ =>{
+        List<Restaurant> allRestaurants = Restaurant.GetAll();
+        return View["restaurants.cshtml", allRestaurants];
+      };
+      Post["/restaurants/new"] = _ =>{
+        string newName = Request.Form["name"];
+        string newFavDish = Request.Form["fav-dish"];
+        DateTime newDate = Request.Form["start-date"];
+
+        Restaurant newRestaurant = new Restaurant(newName, newFavDish, newDate, 1);
+        newRestaurant.Save();
+
+        List<Restaurant> allRestaurants = Restaurant.GetAll();
+        return View["restaurants.cshtml", allRestaurants];
       };
     }
   }
